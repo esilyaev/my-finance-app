d8c69ddf18a686c7c5e225702051ff6a62d40303
Eugeny Silyaev <esilayev@gmail.com>
2022-10-09 19:48:39 +0300

    add axios router and pinia

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import VueRouter from 'vue-router'
+
+const routes = [
+  { path: '/', component:  },
+  { path: '/about', component: About },
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
